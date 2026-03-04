9c064ae853b513cb8e5cfc8194820cd0ebba9efe
Frostflowerss <130375717+Frostflowerss@users.noreply.github.com>
2026-03-04 09:11:12 +0700

    Add files via upload

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export { Header } from "@/components/Header";
+export { Footer } from "@/components/Footer";
+export { Mailchimp } from "@/components/Mailchimp";
+export { ProjectCard } from "@/components/ProjectCard";
+export { HeadingLink } from "@/components/HeadingLink";
+export { RouteGuard } from "@/components/RouteGuard";
+export { Providers } from "@/components/Providers";
+export { ScrollToHash } from "@/components/ScrollToHash";
+export { ThemeToggle } from "@/components/ThemeToggle";
+export { CustomMDX } from "@/components/mdx";
+
+export { HomeShowcase } from "@/components/home/HomeShowcase";
+export { WorkCarousel } from "@/components/work/WorkCarousel";
